9368fb4e0e127152e896f489b89a2237caab983b
Влад <65511709+m1dlace@users.noreply.github.com>
2020-05-20 21:57:33 +0300

    Lol

--- 2 Лаба/5.py
+++ 2 Лаба/5.py
@@ -11,8 +11,13 @@ import os.path
 import re
 
 text = input("Введите текст: ")
-pattern = r'[A-Z]\D+\d{2,4}\b'
+pattern = r'[A-Z]\D+\d{2}\b'
+pattern2 = r'[A-Z]\D+\d{4}\b'
 list_words = re.findall(pattern, text)
+list_words2 = re.findall(pattern2, text)
 
 for i in range(len(list_words)):
     print(list_words[i])
+
+for i in range(len(list_words2)):
+    print(list_words2[i])
